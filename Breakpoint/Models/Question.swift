e5121f29c9c0be2e75f682f20060b4c022a95a79
Ondřej Svojše <ondrejsvojse@ondrej-macbook-pro.local>
2018-02-22 12:58:19 +0100

    First commit

---
+++ Breakpoint/Models/Question.swift
@@ -0,0 +1,57 @@
+//
+//  Question.swift
+//  Breakpoint
+//
+//  Created by Ondřej Svojše on 05.01.18.
+//  Copyright © 2018 Ondřej Svojše. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+    
+}
+
+
+enum ResponseType {
+    
+    case single, multiple, ranged
+}
+
+
+struct Answer {
+    
+    var text: String
+    var type: AnimalType
+}
+
+enum AnimalType: Character {
+    
+    case dog = "🐶", cat = "🐱", rabbit = "🐰" , turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+            
+        case .dog:
+            return "„You are incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends."
+            
+        case .cat:
+            return "Mischievous, yet mild-tempered, you enjoy doing things on your own terms."
+            
+        case .rabbit:
+            return "You love everything that's soft. You are healthy and full of energy."
+            
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        }
+        
+        
+    }
+    
+    
+    
+}
